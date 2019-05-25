d138a6b603e0937f45563ff5edd6ba770c606d66
arun <arun@medzdigital.com>
2019-05-25 16:39:21 +0530

    Initial commit

---
+++ card.js
@@ -0,0 +1,39 @@
+import { url } from './services.js';
+
+class CardComponent extends HTMLElement {
+    constructor (){
+        super();
+        const shadow = this.attachShadow({mode: 'open'});
+
+        // Clone the template so that it can be attched to the
+
+        const template = document.getElementById('card-view');
+        const templateInstance = template.content.cloneNode(true);
+        shadow.appendChild(templateInstance);
+    }
+
+    async connectedCallback (){
+        this.gifObj = await this.fetchFromGiphy();
+        this.render(this, this.gifObj);
+    }
+
+    render(shadowElem, data){
+        const shadowRoot = shadowElem.shadowRoot;
+        shadowRoot.getElementById('card-title').innerHTML = data.name;
+        shadowRoot.getElementById('gif-view').src = data.url;
+    }
+
+    async fetchFromGiphy (){
+        const res = await fetch(url);
+        const json = await res.json();
+        const gifUrl = json['data']['0'].images['fixed_height_small'].url;
+        const gifName = json['data']['0'].title;
+        const gifObject = {
+            name: gifName,
+            url: gifUrl
+        }
+        return gifObject;
+    }
+}
+
+customElements.define('card-component', CardComponent);
---
+++ services.js
@@ -0,0 +1,3 @@
+const API_KEY = 'ltgvJxrNP9K2lfHpHNIvw3unwg9g2ymn';
+const url = `http://api.giphy.com/v1/gifs/trending?api_key=` + API_KEY + `&limit=1`;
+export {API_KEY, url}; // export the url so that i can be used extrnally.
